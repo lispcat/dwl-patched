87395fb02692a99923d708e137cb780bda21768d
lispcat <187922791+lispcat@users.noreply.github.com>
2024-12-25 19:25:07 -0500

    fix for deck, change ROUND to (int)roundf

--- dwl.c
+++ dwl.c
@@ -1475,7 +1475,7 @@ deck(Monitor *m)
 		return;
 
 	if (n > m->nmaster)
-		mw = m->nmaster ? ROUND(m->w.width * m->mfact) : 0;
+		mw = m->nmaster ? (int)roundf(m->w.width * m->mfact) : 0;
 	else
 		mw = m->w.width;
 	i = my = 0;
